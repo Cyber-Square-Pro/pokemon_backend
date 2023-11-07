8f1c222419eda651bc2df41532d4e4a07305a433
PremjithK <premjithkallai@gmail.com>
2023-11-07 16:25:02 +0530

    pokemon module edited

--- src/pokemon/pokemon.controllers.ts
+++ src/pokemon/pokemon.controllers.ts
@@ -1,50 +1,24 @@
-import { Controller, Get, Post, Param } from "@nestjs/common";
-import { Pokemon } from "./pokemon.model";
-
+import { Controller, Get, Param } from '@nestjs/common';
+import { Pokemon } from './pokemon.model';
+import { PokemonService } from './pokemon.services';
 
 @Controller('pokemon')
-export class PokemonController{
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
 
-    // Get one pokemon's details
+  // Get one pokemon's details
   @Get('/:id')
-  getUser(@Param('id') id:number):any{
-    return this.usersService.getOneUser(id)
+  getPokemon(@Param('id') id: number): any {
+    return this.pokemonService.getOnePokemon(id);
   }
 
   // Get all Users Info
   @Get()
-  getAllUsers():any{
-    return this.usersService.getAllUsers()
+  getAllPokemon(): any {
+    return this.pokemonService.getAllpokemon();
   }
 
-  // Add User
-  @Post()
-  createUser(@Body() body:{
-    name:string,
-    email:string,
-    phone:number,
-    password:string,
-  }): any {
-    return this.usersService.createUser(body.name, body.email, body.phone,body.password)
-  }
-
-  // Update a User
-  @Patch(':id')
-  updateUser(
-    @Param('id') id:number, 
-    @Body('name') name:string,
-    @Body('email') email:string,
-    @Body('phone') phone:number,
-    @Body('password') password:string,
-  ){
-    this.usersService.updateUser(id, name,email,phone,password)
-    return null
-  }
-
-  // Delete a User
-  @Delete(':id')
-  deleteUser(@Param('id') id){
-    this.usersService.deleteUser(id)
-    return null
-  }
+  // Add a Pokemon
+  // Update a pokemon
+  // Delete a pokemon
 }
