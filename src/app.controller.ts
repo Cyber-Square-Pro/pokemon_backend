a0d92b2327a4cee2fcdeb03b2bdd9aef00211e93
PremjithK <premjithkallai@gmail.com>
2023-12-14 13:22:42 +0530

    deployment 03

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,12 +7,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @UseGuards(AuthGuard('local'))
-  @Post('auth-test')
-  async login(@Request() req) {
-    console.log(req.query)
-    return 'auth success';
-  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   homeRoute(): string {
-    return 'Welcome to Team B Pokemon API!';
+    return `
+    <center>
+    <h1>Welcome to Pokedex API of Team B</h1>
+    
+    </center>
+    `;
   }
 }
