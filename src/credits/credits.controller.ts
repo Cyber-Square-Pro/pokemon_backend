37f2e985e7a782e9f9b8564deb7d66f97e7997c8
PremjithK <premjithkallai@gmail.com>
2023-12-27 13:34:52 +0530

    created credits controller

--- src/credits/credits.controller.ts
+++ src/credits/credits.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards } from '@nestjs/common';
 import { UserService } from 'src/users/users.service';
 import { CreditsService } from './credits.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('credits')
 export class CreditsController {
@@ -8,26 +9,26 @@ export class CreditsController {
     private readonly userService: UserService,
     private readonly creditService: CreditsService,
   ) {}
-
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async getCredits(@Body() body: { username: string }) {
     const user = await this.userService.findUserByName(body.username);
     const result = await this.creditService.getCredits(user);
     return result.credits;
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post('create')
   async createAccount(@Body() body: { username: string }) {
     const user = await this.userService.findUserByName(body.username);
     return this.creditService.createAccount(user);
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Patch('add')
   async addCredits(@Body() body: { username: string }) {
     const user = await this.userService.findUserByName(body.username);
     return this.creditService.addCredits(user);
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Patch('spend')
   async spendCredits(@Body() body: { username: string; amount: number }) {
     const user = await this.userService.findUserByName(body.username);
